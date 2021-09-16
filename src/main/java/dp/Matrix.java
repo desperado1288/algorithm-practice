3054e63cdf42f19c19d20cfa54bee4df6b6a6d50
Yi Ding <yd2277@columbia.edu>
2021-09-15 21:27:15 -0400

    add dp sol

--- src/main/java/dp/Matrix.java
+++ src/main/java/dp/Matrix.java
@@ -3,19 +3,26 @@ package dp;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Arrays;
+
 public class Matrix {
     private final Logger logger = LogManager.getLogger(Matrix.class);
 
+
     // 1. 任何一个状态互相不依赖; 2. dfs的参数跟dp状态一致
+    // ----------------------------------------------------------------
+    // 有墙矩阵从第一行到最后一行的最长距离
     int[][][] dp;
     boolean[][] visited;
     int[] directions = {0, 1, 0, -1};
     int maxI = 0, timeComplexityCount = 0;
-    // 有墙矩阵从第一行到最后一行的最长距离
+    // dfs with memorization f(i,j,d). d: 3 directions (0,1,2).
+    // time complexity: O(3MN), space: O(3MN)
     public int longestPathInDiretion3dfs(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
+        maxI = 0;
+        timeComplexityCount = 0;
         dp = new int[m][n][3];
-        visited = new boolean[m][n];
         int max = 0;
         for (int i = 0; i < n; i++) {
             if (matrix[0][i] == 0)
@@ -27,11 +34,10 @@ public class Matrix {
         return maxI == m - 1 ? max : 0;
     }
 
+    // visited no need because dfs with direction
     private int dfs(int[][] matrix, int i, int j, int d) {
-        if (i < 0 || j < 0 || i == matrix.length || j == matrix[0].length || visited[i][j] || matrix[i][j] != 0)
+        if (i < 0 || j < 0 || i == matrix.length || j == matrix[0].length || matrix[i][j] != 0)
             return 0;
-        // visited no need because dfs with direction
-//        visited[i][j] = true;
         if (dp[i][j][d] > 0)    return dp[i][j][d];
         timeComplexityCount++;
         maxI = Math.max(maxI, i);
@@ -46,12 +52,46 @@ public class Matrix {
         return res + 1;
     }
 
+    // dp solution.  each row go left and right once, then move to next row.
+    // time complexity: O(5MN), space: O(MN) can be reduced to O(N)
+    public int longestPathInDiretion3dp(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        timeComplexityCount = 0;
+        int[][] dp = new int[m][n+2];
+        int max = 0;
+        // initialize first line as 1
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0)
+                dp[0][i+1] = 1;
+        }
+        for (int i = 0; i < m; i++) {
+            int[] left = Arrays.copyOf(dp[i], n + 2), right = Arrays.copyOf(dp[i], n + 2);
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0)
+                    right[j+1] = Math.max(right[j+1], right[j] + 1);
+                if (matrix[i][n-1-j] == 0)
+                    left[n-j] = Math.max(left[n-j], left[n-j+1] + 1);
+            }
+            for (int j = 0; j < n; j++) {
+                dp[i][j + 1] = Math.max(left[j + 1], right[j + 1]);
+                if (i < m - 1 && matrix[i+1][j] == 0)
+                    dp[i+1][j+1] = dp[i][j+1] + 1;
+                else if (i == m - 1)
+                    max = Math.max(max, dp[i][j+1]);
+            }
+        }
+        return max;
+    }
+    // -----------------------------------------------------------
+
     public static void main(String[] args) {
         System.out.println("ko");
         Matrix matrix = new Matrix();
         int[][] m = {{0,0,0,1,0,0}, {0,1,0,0,0,1}, {0,0,0,1,1,1}};
         System.out.println(matrix.longestPathInDiretion3dfs(m) == 8);
+        System.out.println(matrix.longestPathInDiretion3dp(m) == 8);
         m = new int[][] {{0,0,0,1,0,0}, {0,1,0,0,0,1}, {1,1,1,1,1,1}};
         System.out.println(matrix.longestPathInDiretion3dfs(m) == 0);
+        System.out.println(matrix.longestPathInDiretion3dp(m) == 0);
     }
 }
